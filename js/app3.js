3ab2433cd2b91a6b81240dec042ed170983215ab
Eric Harrison <github@hrrsn.co>
2021-10-07 19:57:23 -0400

    Improved our game code.

--- js/app3.js
+++ js/app3.js
@@ -1,62 +1,106 @@
 var game = {
-	win: 0,
-	loss: 0,
-	pastResults: []
+	won: 0,
+	lost: 0,
+	tie: 0,
+	pastResults: {
+		paper: 0,
+		rock: 0,
+		scissors: 0
+	},
+	winRate: {
+		paper: 0,
+		rock: 0,
+		scissors: 0
+	},
+	resultEl: document.getElementById('results')
 };
 
+
+// function to kick off a game
 function playGame(choice) {
 
-	var choices = [ 'paper', 'rock', 'scissors' ];
+	var compChoice = getComputerChoice();
+	
+	var result = getWinner(choice, compChoice);
 
-	var random2 = Math.floor(Math.random() * 3);
+	processResults(choice, result);
 
-	var bot1 = choice;
-	var bot2 = choices[random2];
-
-	console.log(choice);
-
-	if ( bot1 == 'paper' ) {
-
-		if ( bot2 == 'scissors' ) {
-			game.loss = 1;
-		} else if ( bot2 == 'rock' ) {
-			game.win = 1;
-		}
-
-	} else if ( bot1 == 'rock' ) {
-
-		if ( bot2 == 'paper' ) {
-			game.loss = 1;
-		} else if ( bot2 == 'scissors' ) {
-			game.win = 1;
-		}
-
-	} else if ( bot1 == 'scissors' ) {
-
-		if ( bot2 == 'rock' ) {
-			game.loss = 1;
-		} else if ( bot2 == 'paper' ) {
-			game.win = 1;
-		}
-
-	}
-
-	var results = '';
-
-	if ( game.win ) {
-		results = bot1 + ' beats ' + bot2 + ' - YOU WIN!';
-	} else if ( game.loss ) {
-		results = bot2 + ' beats ' + bot1 + ' - You lose. :(';
-	} else {
-		results = 'Tie game';
-	}
-
-	game.win = 0;
-	game.loss = 0;
-
-	var pastResult = document.getElementById('results').innerHTML;
-
-	document.getElementById('pastResults').innerHTML = '<li>' + results + '</li>' + document.getElementById('pastResults').innerHTML;
-
-	document.getElementById('results').innerHTML = results;
 }
+
+// function to pick the computer choice
+function getComputerChoice() {
+	var options = [
+		'paper', 'rock', 'scissors'
+	];
+
+	return options[ 
+		Math.floor(Math.random() * options.length) 
+	];
+}
+
+function getPaperLoverChoice() {
+	var options = [ 
+		'paper', 'rock'
+	];
+
+	return options[ 
+		Math.floor(Math.random() * options.length) 
+	];
+}
+
+// function to decide who won?
+function getWinner(playerChoice, computerChoice) {
+	var result = 0;
+
+	if ( playerChoice == 'paper' ) {
+
+		if ( computerChoice == 'scissors' ) {
+			result = -1;
+		} else if ( computerChoice == 'rock' ) {
+			result = 1;
+		}
+
+	} else if ( playerChoice == 'rock' ) {
+
+		if ( computerChoice == 'paper' ) {
+			result = -1;
+		} else if ( computerChoice == 'scissors' ) {
+			result = 1;
+		}
+
+	} else if ( playerChoice == 'scissors' ) {
+
+		if ( computerChoice == 'rock' ) {
+			result = -1;
+		} else if ( computerChoice == 'paper' ) {
+			result = 1;
+		}
+
+	}
+	
+	// return the winner [-1/0/1]	
+	return result;
+}
+
+// function to save results and display them
+function processResults(playerChoice, result) {
+	// paper // rock // scissors
+	// store the choice in our game object pastResults
+	game.pastResults[ playerChoice ]++;
+
+	if ( result == -1 ) {
+		game.lost++;
+		game.resultEl.innerHTML = 'Lost';
+	} else if ( result == 1 ) {
+		game.won++;
+		game.winRate[ playerChoice ]++;
+		game.resultEl.innerHTML = 'Won';
+	} else {
+		game.tie++;
+		game.resultEl.innerHTML = 'Tie';
+	}
+
+}
+
+// TODO - consider splitting out the displayResults() 
+
